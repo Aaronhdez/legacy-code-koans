34de2deb20397d306336e9f4541c6411ba465f44
Aarón Hernández Álvarez <aahernandez@domingoalonsogroup.com>
2023-05-26 10:14:32 +0100

    Refactor: Introduced mock dependency

---
+++ src/LegacyCodeKoans/Hardwired/MockedDependency.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace LegacyCodeKoans.Hardwired {
+    class MockedDependency : IDependency {
+        public void SomeSideEffect() {
+            Console.WriteLine("Dummy mock method");
+        }
+    }
+}
